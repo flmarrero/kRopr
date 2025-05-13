c25653d69083b0788ead5ad892bc9a23c88afacf
Florian Marrero Liestmann <florian.buestgens@eu.panasonic.com>
2025-05-13 09:06:12 +0000

    Restructure project into userland and kernel

--- flake.nix
+++ flake.nix
@@ -22,16 +22,16 @@
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        inputsFrom = inputs.metaflakes.devShells.${system}.kernel.buildInputs;
+        inputsFrom = inputs.metaflakes.devShells.${system}.kernel.buildInputs ++
+                        inputs.metaflakes.devShells.${system}.rust.buildInputs;
 
         C_INCLUDE_PATH="${pkgs.linuxHeaders}/include";
 
         nativeBuildInputs = with pkgs; [
                 linuxHeaders
-                
         ];
       };
 
-      packages.${system}.test-driver = import ./test/default.nix { inherit pkgs; };
+      packages.${system}.kernel = import ./kernel/default.nix { inherit pkgs; };
     };
 }
